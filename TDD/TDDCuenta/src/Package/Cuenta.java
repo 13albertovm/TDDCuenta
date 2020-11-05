76f1d84e89d974f93af938216f92bdd98fae9a64
13albertovm <67682276+13albertovm@users.noreply.github.com>
2020-11-05 16:57:18 +0100

    V1. Coding

---
+++ TDD/TDDCuenta/src/Package/Cuenta.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Package;
+
+import java.util.List;
+
+/**
+ *
+ * @author injun
+ */
+public class Cuenta {
+    
+    protected String mNumero;
+    protected String mTitular;
+    protected List<Movimiento> mMovimientos;
+    private double Saldo;
+
+    public Cuenta(String numero, String titular) {
+        mNumero = numero;
+        mTitular = titular;
+        mMovimientos = new Vector();
+        Saldo=0;
+    }
+
+    public void ingresar(double x) throws Exception {
+        if (x <= 0) {
+            throw new Exception("No se puede ingresar una cantidad negativa");
+        }
+        Movimiento m = new Movimiento();
+        m.setConcepto("Ingreso en efectivo");
+        m.setImporte(x);
+        this.mMovimientos.add(m);
+    }
+
+    public void retirar(double x) throws Exception {
+        if (x <= 0) {
+            throw new Exception("No se puede retirar una cantidadn negativa");
+        }
+        if (getSaldo() < x) {
+            throw new Exception("Saldo insuficiente");
+        }
+        Movimiento m = new Movimiento();
+        m.setConcepto("Retirada de efectivo");
+        m.setImporte(-x);
+        this.mMovimientos.add(m);
+    }
+
+    /**
+     * @return the Saldo
+     */
+    public double getSaldo() {
+        return Saldo;
+    }
+
+    /**
+     * @param Saldo the Saldo to set
+     */
+    public void setSaldo(double Saldo) {
+        this.Saldo = Saldo;
+    }
+}
---
+++ TDD/TDDCuenta/src/Package/Movimiento.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Package;
+
+/**
+ *
+ * @author usuario
+ */
+public class Movimiento {
+    private String concepto;
+    private double importe;
+    
+    public Movimiento(){}
+
+    /**
+     * @return the concepto
+     */
+    public String getConcepto() {
+        return concepto;
+    }
+
+    /**
+     * @param concepto the concepto to set
+     */
+    public void setConcepto(String concepto) {
+        this.concepto = concepto;
+    }
+
+    /**
+     * @return the importe
+     */
+    public double getImporte() {
+        return importe;
+    }
+
+    /**
+     * @param importe the importe to set
+     */
+    public void setImporte(double importe) {
+        this.importe = importe;
+    }
+    
+}
